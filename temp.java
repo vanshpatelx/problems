547bf59237e13e41270ff97220dadc90adc7576c
vanshpatel <vanshpatel1299@gmail.com>
2022-08-08 16:17:52 +0530

    clean

---
+++ temp.java
@@ -0,0 +1,9 @@
+public class temp {
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "def";
+        String str = "";
+        a += b;
+       System.out.print(a);
+    }
+}
